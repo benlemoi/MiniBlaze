8bc5e118789c998e7d10becfaa43aa18346dde4f
Ben <benjamin.lemoine50@gmail.com>
2017-01-25 11:38:20 +0100

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ prj = VUnit.from_argv()
 root = dirname(__file__)
 
 udp_ip_lib = prj.add_library('miniblaze')
-udp_ip_lib.add_source_files(join(root, 'src', 'hw1', '*.vhd'))
-udp_ip_lib.add_source_files(join(root, 'test', 'Simu', '*.vhd'))
+udp_ip_lib.add_source_files(join(root, 'src', 'hw1', 'generic_hdl_fifo.vhd'))
+udp_ip_lib.add_source_files(join(root, 'test', 'Simu', 'tb_generic_hdl_fifo.vhd'))
 
 prj.main()
