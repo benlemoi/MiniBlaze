e275d2066f7d6dc227cb8b3a3a867d2cc9f52459
Ben <benjamin.lemoine50@gmail.com>
2017-01-26 17:21:04 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,10 +2,11 @@ from vunit import VUnit
 from os.path import join, dirname
 
 prj = VUnit.from_argv()
+prj.enable_location_preprocessing()
 root = dirname(__file__)
 
-udp_ip_lib = prj.add_library('miniblaze')
-udp_ip_lib.add_source_files(join(root, 'src', 'hw1', 'generic_hdl_fifo.vhd'))
-udp_ip_lib.add_source_files(join(root, 'test', 'Simu', 'tb_generic_hdl_fifo.vhd'))
+miniblaze_lib = prj.add_library('miniblaze')
+miniblaze_lib.add_source_files(join(root, 'src', 'hw1', 'generic_hdl_fifo.vhd'))
+miniblaze_lib.add_source_files(join(root, 'test', 'Simu', 'tb_generic_hdl_fifo.vhd'))
 
 prj.main()
