d8a5aac244a316ba8367b566a04a0e84549dbcbb
Ben <benjamin.lemoine50@gmail.com>
2017-02-01 15:56:12 +0100

    Update run.py

--- run.py
+++ run.py
@@ -7,6 +7,10 @@ root = dirname(__file__)
 
 miniblaze_lib = prj.add_library('miniblaze')
 miniblaze_lib.add_source_files(join(root, 'src', 'hw1', 'generic_hdl_fifo.vhd'))
+miniblaze_lib.add_source_files(join(root, 'src', 'hw1', 'ALU.vhd'))
+miniblaze_lib.add_source_files(join(root, 'src', 'hw1', 'ALU_pkg.vhd'))
 miniblaze_lib.add_source_files(join(root, 'test', 'Simu', 'tb_generic_hdl_fifo.vhd'))
+miniblaze_lib.add_source_files(join(root, 'test', 'Simu', 'tb_ALU.vhd'))
+miniblaze_lib.add_source_files(join(root, 'test', 'Simu', 'data_pkg.vhd'))
 
 prj.main()
